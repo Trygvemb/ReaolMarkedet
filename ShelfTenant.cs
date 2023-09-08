b4cd2c5af5d6ca2b0a897cd2260667e6c5a35b8d
Trygve Myhre Bechsgaard <tmbe54131@edu.ucl.dk>
2023-09-08 10:35:07 +0800

    new setup for ShelfTenant

--- ShelfTenant.cs
+++ ShelfTenant.cs
@@ -8,61 +8,47 @@ namespace ReaolMarkedet
 {
     internal class ShelfTenant
     {
-        private string tenantId;
-        private string firstName;
-        private string lastName;
-        private string email;
-        private string accountDetails;
-        private string phone; // Change to string
-        private double totalSale = 0;
+        public string TenantId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public double TotalSale { get; private set; }
+        private string BankAccountDetails { get; set; }
         private static int count = 0;
-
-        public string TenantId
-        {
-            get { return tenantId; }
-            set { tenantId = value; }
-        }
-        public string FirstName
-        {
-            get { return firstName; }
-            set { firstName = value; }
-        }
-        public string LastName
-        {
-            get { return lastName; }
-            set { lastName = value; }
-        }
-        public string Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
-        public string AccountDetails
-        {
-            get { return accountDetails; }
-            set { accountDetails = value; }
-        }
-        public string Phone // Change to string
-        {
-            get { return phone; }
-            set { phone = value; }
-        }
-        public double TotalSale
-        {
-            get { return totalSale; }
-            set { totalSale = value; }
-        }
-
-        public ShelfTenant(string firstName, string lastName, string email, string phone) // Change parameter type to string
+        
+        // constructor for ShelfTenant that takes all parameters
+        public ShelfTenant(string firstName, string lastName, string email, string phone, string bankAccountDetails)
         {
             count++;
             TenantId = $"T{count}";
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.email = email;
-            this.phone = phone;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+            BankAccountDetails = bankAccountDetails;
+            TotalSale = 0;
         }
-
-
+        // overload for constructor that dosnt take bankaccountdeails
+        public ShelfTenant(string firstName, string lastName, string email, string phone)
+        {
+            count++;
+            TenantId = $"T{count}";
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+            TotalSale = 0;
+        }
+        // retreive BankAccountDetails can implement decryption later on
+        public string GetBankAccountDetails()
+        {
+            return BankAccountDetails;
+        }
+        // sets BankAccountDetails can implement encryption later on 
+        public void SetBankAccountDetails(string bankAccountDetails)
+        {
+            BankAccountDetails = bankAccountDetails;
+        }
     }
 }
