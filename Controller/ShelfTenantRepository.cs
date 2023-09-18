b4bced1a0ad4efa4551d4d5e7d3206c85c193828
Trygve Myhre Bechsgaard <tmbe54131@edu.ucl.dk>
2023-09-18 11:51:01 +0800

    CRUD implementation in ShelfTenantRepository

--- Controller/ShelfTenantRepository.cs
+++ Controller/ShelfTenantRepository.cs
@@ -9,5 +9,39 @@ namespace ReaolMarkedet.Controller
 {
     internal class ShelfTenantRepository
     {
+        private List<ShelfTenant> shelfTenants;
+
+        public ShelfTenantRepository()
+        {
+            this.shelfTenants = new List<ShelfTenant>();
+        }
+
+        public void AddShelTenant(ShelfTenant shelfTenant)
+        {
+            shelfTenants.Add(shelfTenant);
+        }
+
+        public ShelfTenant GetShelfTenant(string tenantID)
+        {
+            foreach (ShelfTenant shelfTenant in shelfTenants)
+            {
+                if (shelfTenant.TenantId == tenantID)
+                {
+                    return shelfTenant;
+                }
+            }
+            return null;
+        }
+
+        public void RemoveShelfTenant(string tenantID)
+        {
+            foreach (ShelfTenant shelfTenant in shelfTenants)
+            {
+                if (shelfTenant.TenantId == tenantID)
+                {
+                    shelfTenants.Remove(shelfTenant);
+                }
+            }
+        }
     }
 }
