3ac9ca0f79d7608e13e947b44905f852bfc5fddf
Trygve Myhre Bechsgaard <tmbe54131@edu.ucl.dk>
2023-09-18 14:07:57 +0800

    controller fields and constructor implemented

---
+++ Controller/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaolMarkedet.Controller
+{
+    internal class Controller
+    {
+        private BarcodeRepository barcodeRepository;
+        private ShelfTenantRepository ShelfTenantRepository;
+        private PayoutRepository payoutRepository;
+
+        public Controller()
+        {
+            this.barcodeRepository = new BarcodeRepository();
+            this.ShelfTenantRepository = new ShelfTenantRepository();
+            this.payoutRepository = new PayoutRepository();
+        }
+
+
+    }
+}
